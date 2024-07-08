e856aa469e5a31be5b666a5e32685cd9c9af5230
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2024-07-08 19:12:44 +0400

    safe integration

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './safe-pimlico.jsx';
+import './index.css';
+
+// Use ReactDOM.createRoot to render your app
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
